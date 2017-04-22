db100274563bc97cdc625d9df7d7bff7cc73c46f
Berrak Nil <berraknil@gmail.com>
2017-04-22 15:40:55 +0300

    home page title accepted

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,8 +3,11 @@ import './Home.css'
 
 const Home = () => (
   <main className="home">
-    <h1 className="home__heading">Linear</h1>
-    <h1 className="home__subheading">Perspectives</h1>
+    <h1>linear perspective</h1>
+    <h4>noun</h4>
+    <h4>
+      a type of perspective used by artists, in which the relative size, shape, and position of objects is determined by drawn or imagined lines converging at a point on the horizon.
+    </h4>
   </main>
 )
 
