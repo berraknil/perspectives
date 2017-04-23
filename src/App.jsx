f0d6017d8beb4aca82cefa7daa41cdc41d9cbda6
Berrak Nil <berraknil@gmail.com>
2017-04-23 08:52:08 +0300

    secondary menu working correctly, pre-refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,18 @@ const App = () => (
   <Router>
     <main className="main">
       <nav className="navigation">
+        <ul className="navigation__list--mobile">
+          <li><NavLink activeClassName="active" exact to="/">Home</NavLink></li>
+          <li><NavLink activeClassName="active" to="/one">1</NavLink></li>
+          <li><NavLink activeClassName="active" to="/two">2</NavLink></li>
+          <li><NavLink activeClassName="active" to="/three">3</NavLink></li>
+          <li><NavLink activeClassName="active" to="/about">About</NavLink></li>
+        </ul>
         <ul className="navigation__list">
           <li><NavLink activeClassName="active" exact to="/">Home</NavLink></li>
-          <li><NavLink activeClassName="active" to="/one">One</NavLink></li>
-          <li><NavLink activeClassName="active" to="/two">Two</NavLink></li>
-          <li><NavLink activeClassName="active" to="/three">Three</NavLink></li>
+          <li><NavLink activeClassName="active" to="/one">One-Point Perspective</NavLink></li>
+          <li><NavLink activeClassName="active" to="/two">Two-Point Perspective</NavLink></li>
+          <li><NavLink activeClassName="active" to="/three">Three-Point Perspective</NavLink></li>
           <li><NavLink activeClassName="active" to="/about">About</NavLink></li>
         </ul>
       </nav>
