b295ab7f90f484ffeee3062a997c45a431caa777
Berrak Nil <berraknil@gmail.com>
2017-04-25 12:32:45 +0300

    about page done, page sizing flex removed

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,17 +3,16 @@ import './About.css'
 
 const About = () => (
   <div className="about">
-    <div className="about__text">
-      <h1>About</h1>
+    <div className="about__text">    
       <h4>
-        Built with:
+        Built with
         {' '}
         <a
           href="https://facebook.github.io/react/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          React v15.5
+          React
         </a>
         ,
         {' '}
@@ -22,39 +21,26 @@ const About = () => (
           target="_blank"
           rel="noopener noreferrer"
         >
-          React-Router v4
-        </a>
-      </h4>
-      <h4>
-        Text Source:
-        {' '}
-        <a
+          React-Router
+        </a>, <br /> all text is taken from <a
           href="https://en.wikipedia.org/wiki/Perspective_(graphical)#Types_of_perspective"
           target="_blank"
           rel="noopener noreferrer"
         >
           Wikipedia
-        </a>
-      </h4>
-      <h4>
-        Source Code:
-        {' '}
-        <a
+        </a>, <br />source code is available on <a
           href="https://github.com/berraknil/perspectives"
           target="_blank"
           rel="noopener noreferrer"
         >
           GitHub
-        </a>
-      </h4>
-      <h4>
-        <a
+        </a> <br /> contact me <a
           href="https://berraknil.github.io/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Contact
-        </a>
+        here
+        </a>.
       </h4>
     </div>
   </div>
