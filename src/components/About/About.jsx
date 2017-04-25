d82b0aaa28726784041251fdf0c15197ba5df709
Berrak Nil <berraknil@gmail.com>
2017-04-25 11:38:00 +0300

    about page done

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,63 @@
 import React from 'react'
-import Content from '../Content'
+import './About.css'
 
 const About = () => (
-  <Content>
-    <h3>ABOUT</h3>
-  </Content>
-  )
+  <div className="about">
+    <div className="about__text">
+      <h1>About</h1>
+      <h4>
+        Built with:
+        {' '}
+        <a
+          href="https://facebook.github.io/react/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React v15.5
+        </a>
+        ,
+        {' '}
+        <a
+          href="https://reacttraining.com/react-router/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React-Router v4
+        </a>
+      </h4>
+      <h4>
+        Text Source:
+        {' '}
+        <a
+          href="https://en.wikipedia.org/wiki/Perspective_(graphical)#Types_of_perspective"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Wikipedia
+        </a>
+      </h4>
+      <h4>
+        Source Code:
+        {' '}
+        <a
+          href="https://github.com/berraknil/perspectives"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+      </h4>
+      <h4>
+        <a
+          href="https://berraknil.github.io/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Contact
+        </a>
+      </h4>
+    </div>
+  </div>
+)
 
 export default About
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,6 @@ const Home = () => (
   <div className="home">
     <div className="home__text">
       <h1>linear perspective</h1>
-      <h4>noun</h4>
       <h4>
         a type of perspective used by artists, in which the relative size, shape, and position of objects is determined by drawn or imagined lines converging at a point on the horizon.
       </h4>
